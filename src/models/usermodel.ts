4f868b3f6034fd29c1a9f47eb79167c83d4f8e3d
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-07-20 20:06:33 +0300

    implement user model and use the exist user model because of HRM behaviour of Nextjs

---
+++ src/models/usermodel.ts
@@ -0,0 +1,30 @@
+import { Schema, model, models } from "mongoose";
+import validator from "validator";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: [true, "Email exist"],
+    validate: {
+      validator: function (v: string) {
+        return validator.isEmail(v);
+      },
+    },
+  },
+  username: {
+    type: String,
+    default: "USERNAME",
+    required: [true, "Email is required"],
+    match: [
+      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
+    ],
+  },
+  image: {
+    type: String,
+  },
+});
+userSchema.index({ email: 1 }, { unique: true });
+const User = models.User || model("User", userSchema); //  because HRM of nextjs
+export default User;
